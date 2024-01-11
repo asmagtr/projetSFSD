20bd954220fefc57b6ed6faadf0c17b11dbf24e4
asmagtr <gtrasma@gmail.com>
2024-01-11 23:12:19 +0100

    ajout des fct "ouvrir"/"fermer"/"afficherEtudiant" et j'ai aussi ajoutéun champs nbr d'enregistrement effacés logiquement dans l'aentete d'un fichier

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ typedef struct bloc buffer;// le buffer est de meme type que le bloc
 typedef struct entete{
     int nbrBlocs;
     int nbrEl; //nombre d'element (enregistrement)
+    int nbrElSupp; // nombre d'enregistrement effacé logiquement
 }entete;
 // puis on definit le fichier
 typedef struct TOF{
@@ -30,7 +31,32 @@ typedef struct TOF{
     FILE *f
 }TOF;
 ///****************les algorithmes de manipulation des fichiers*********************//////
+/// Ouvrir le fichier logique et l’associer au fichier physique en précisant si le fichier est nouveau ('N') ou ancien ('A').
+TOF* ouvrir(char fileName[30],char mode){
+    TOF* fileTOF=malloc(sizeof(TOF));
+        if( mode=='a'|| mode=='A'){
+            fileTOF->f=fopen(fileName,"rb+");
+            fread(&(fileTOF->tete),sizeof(entete),1,fileTOF->f);
+        }
+        else{
+            fileTOF->f=fopen(fileName,"wb+");
+            (fileTOF->tete).nbrBlocs=(fileTOF->tete).nbrEl=(fileTOF->tete).nbrElSupp=0;/// on initialise le nbr de blocs et d'enregistrement effacées et non effacé á 0 
+            fwrite(&(fileTOF->tete),sizeof(entete),1,fileTOF->f); /// on copie l'entete dans notre fichier
+        }
+    return fileTOF;
+}
+// Fermer le fichier.
+void fermer(TOF *F){
+    rewind(F->f);// on se met dans le debut du fichier
+    fwrite(&(F->tete),sizeof(F->tete),1,F->f);// on update l'entete dans le fichier logique (pour savoir avec quoi nous travaillons plus tard)
+    fclose(F->f);
+    F=NULL;
+}
 
+// une fonction qui affiche les données d'un etudiant 
+void afficherEtudiant(enregistrement e){
+    printf("le nom :%s / le prenom : %s / le matricule : %d \n",e.nom,e.prenom,e.matricule);
+}
 
 
 int main(){
